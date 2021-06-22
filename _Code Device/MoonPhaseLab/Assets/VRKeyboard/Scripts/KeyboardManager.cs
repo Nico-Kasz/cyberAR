ef164ea64f8c40c829fe7f520c41b366d842963f
Nicok01 <42388389+Nicok01@users.noreply.github.com>
2021-06-22 14:22:33 -0500

    added Keybaord
    imported VR keybaord and modded script

---
+++ _Code Device/MoonPhaseLab/Assets/VRKeyboard/Scripts/KeyboardManager.cs
@@ -0,0 +1,109 @@
+﻿/***
+ * Author: Yunhan Li
+ * Any issue please contact yunhn.lee@gmail.com
+ ***/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace VRKeyboard.Utils
+{
+    public class KeyboardManager : MonoBehaviour
+    {
+        #region Public Variables
+        [Header("User defined")]
+        [Tooltip("If the character is uppercase at the initialization")]
+        public bool isUppercase = false;
+        public int maxInputLength;
+
+        [Header("Text Boxes")]
+        public Text usr_text;
+        public Text pas_text; 
+
+        [Header("Essentials")]
+        public Transform keys;
+        #endregion
+
+        #region Private Variables
+        private Text currText;
+        private string Input 
+        {
+            get { return "inputText.text"; }
+            set { usr_text.text = value; }
+        }
+        private Key[] keyList;
+        private bool capslockFlag;
+        #endregion
+
+        #region Monobehaviour Callbacks
+        void Awake()
+        {
+            keyList = keys.GetComponentsInChildren<Key>();
+        }
+
+        void Start()
+        {
+            foreach (var key in keyList)
+            {
+                key.OnKeyClicked += GenerateInput;
+            }
+            capslockFlag = isUppercase;
+            CapsLock();
+        }
+        #endregion
+
+        #region Public Methods
+        public void Backspace()
+        {
+            if (Input.Length > 0)
+            {
+                Input = Input.Remove(Input.Length - 1);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void Clear()
+        {
+            Input = "";
+        }
+
+        public void CapsLock()
+        {
+            foreach (var key in keyList)
+            {
+                if (key is Alphabet)
+                {
+                    key.CapsLock(capslockFlag);
+                }
+            }
+            capslockFlag = !capslockFlag;
+        }
+
+        public void Shift()
+        {
+            foreach (var key in keyList)
+            {
+                if (key is Shift)
+                {
+                    key.ShiftKey();
+                }
+            }
+        }
+
+        public void GenerateInput(string s)
+        {
+            if (Input.Length > maxInputLength) { return; }
+            Input += s;
+        }
+
+        public void setText(int val) 
+        {
+
+        }
+        #endregion
+    }
+}
