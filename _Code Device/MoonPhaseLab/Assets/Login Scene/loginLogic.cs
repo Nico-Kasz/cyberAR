975f223ae86964910d160b39e0eadca8a8fa0ac2
Nicok01 <42388389+Nicok01@users.noreply.github.com>
2021-06-24 15:01:45 -0500

    Animation and script fixes

--- _Code Device/MoonPhaseLab/Assets/Login Scene/loginLogic.cs
+++ _Code Device/MoonPhaseLab/Assets/Login Scene/loginLogic.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class loginLogic : MonoBehaviour
 {
     #region Public Variables
+    public GameObject LoginUI; 
     public GameObject usr;
     public GameObject pas;
     public GameObject loading;
+    public GameObject intro; 
+    #endregion
+
+    #region Private Variables 
+    private bool UIStarted;
     #endregion
 
     #region MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UIStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!UIStarted && intro.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            intro.SetActive(false);
+            StartLoginUI();
+        }
     }
     #endregion
 
@@ -43,7 +53,14 @@ public class loginLogic : MonoBehaviour
             pas.SetActive(false);
             loading.SetActive(false);
         }
+    }
+    #endregion
 
+    #region Private Events
+    private void StartLoginUI()
+    {
+        UIStarted = true;
+        LoginUI.SetActive(true);
     }
     #endregion
 }
