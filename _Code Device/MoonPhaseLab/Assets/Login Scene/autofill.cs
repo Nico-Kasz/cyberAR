d19c37fc6abe5bd42b2ea2b161c377c0b872ddac
Nicok01 <42388389+Nicok01@users.noreply.github.com>
2021-07-02 11:27:46 -0500

    Dropbox almost works - pre move
    Moving to child object - pre move save

--- _Code Device/MoonPhaseLab/Assets/Login Scene/autofill.cs
+++ _Code Device/MoonPhaseLab/Assets/Login Scene/autofill.cs
@@ -9,8 +9,9 @@ public class autofill : MonoBehaviour
     public bool testingVersion;
 
     #region Private Variables
-    private string[] names;
+    private List<string> names = new List<string>();
     private string text;
+    private Dropdown dropdown;
     #endregion
 
 
@@ -18,7 +19,10 @@ public class autofill : MonoBehaviour
     void Start()
     {
         text = transform.GetChild(0).GetComponent<Text>().text;
-        names = new string[4] { "jim", "sam", "chad", "cronie" };
+        string[] nameslist = new string[] { "jim", "sam", "chad", "cronie", "crestin", "crestolemu", "crester", "crestunker"};
+        foreach (string name in nameslist) { names.Add(name); }
+        dropdown = GetComponent<Dropdown>();
+
         queryAndUpdate();
         printNames();
 
@@ -34,8 +38,8 @@ public class autofill : MonoBehaviour
 
             if (testingVersion)
             {
-                sort();
-                setOptions();
+                sort(names);
+                setOptions(names);
             }
             else
             {
@@ -50,34 +54,39 @@ public class autofill : MonoBehaviour
         foreach (string str in names) { print(str); }
     }
 
+    // TODO: FIX THIS 
     public void queryAndUpdate()
     {
+        List<string> strings = new List<string>();
         if (text.Length >= 3)
         {
+            dropdown.Show();
             // send Qury and retrieve new names list
             // foreach (string name in csv) { if (name.contains(name) ) List.Add(name);}
             // names = new names :)
-            List<string> strings = new List<string>();
-            strings.Add("jeff");
-            for (int i = 0; i < strings.Count; i++) { names[i] = strings[i]; }
-            // Update
-            sort();
-            setOptions();
+            foreach (string name in names) { if (name.Contains(text)) { strings.Add(name); } }
         }
+        else
+        {
+            dropdown.Hide();
+        }
+        // Update
+        sort(strings);
+        setOptions(strings);
+        dropdown.RefreshShownValue();
     }
     #endregion
 
     #region Private Methods 
-    private void sort()
+    private void sort(List<string> names)
     {
-        Array.Sort(names, (s1, s2) => String.Compare(s1, s2));
+        names.Sort();  // used for List<string> implementation 
+        // Array.Sort(names, (s1, s2) => String.Compare(s1, s2));   // used for string[] implementation
     }
 
-    private void setOptions()
+    private void setOptions(List<string> names)
     {
-        var dropdown = GetComponent<Dropdown>();
         dropdown.options.Clear();
-        transform.GetChild(0).GetComponent<Text>().text = text;
         dropdown.options.Add(new Dropdown.OptionData(text)); // might delete this line depending on how this works - repetetive 
         foreach (string name in names)
         {
